94169a8f649bcb6630d0a4cb3016ab2329365a84
elijahmm17 <elijahmm17@gmail.com>
2019-07-17 19:03:52 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,8 +2,12 @@ require './environment'
 
 module FormsLab
   class App < Sinatra::Base
-
+    
     get '/' do 
+      erb :root
+    end 
+
+    get '/new' do 
       erb :'pirates/new' 
     end 
     
