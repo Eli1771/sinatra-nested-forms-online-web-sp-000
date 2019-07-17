46da661c326e53e1cb5edf753f473144188492b5
elijahmm17 <elijahmm17@gmail.com>
2019-07-17 19:00:41 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -10,7 +10,7 @@ module FormsLab
     post '/pirates' do 
       @pirate = Pirate.new(params[:pirate]) 
       
-      params[:pirate][:ships][]
+      params[:pirate][:ships][].each
       erb :'pirates/show'
     end 
   end
